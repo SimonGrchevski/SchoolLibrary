bbaf287b88ad56460637d7a974fc48ebb8443ce5
SimonGrchevski <simongrcevski@gmail.com>
2021-10-04 17:11:49 +0200

    Create the person class

---
+++ person.rb
@@ -0,0 +1,17 @@
+class Person
+  attr_accessor :name, :age
+  attr_writer :id
+  def initialize(age,name='unknown',parent_permission=true)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def is_of_age?
+    age >=18
+  end
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+end
