0c4e73f8aecebfacf553cad98828dbc1c6b5262a
SimonGrchevski <simongrcevski@gmail.com>
2021-10-04 18:54:21 +0200

    Add id instance variable to t he person class

--- person.rb
+++ person.rb
@@ -1,11 +1,12 @@
 class Person
   attr_accessor :name, :age
-  attr_writer :id
+  attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @id = Random.rand(0..5000)
   end
 
   def of_age?
